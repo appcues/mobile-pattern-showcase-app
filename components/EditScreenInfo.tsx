5c2047fe50a4f95ae1fc3fda17a37f5c1576fada
Matt Hayashida <matt.hayashida@appcues.com>
2023-09-05 14:24:03 -0400

    ✨ Add Mulish font

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 
 import Colors from '../constants/Colors';
 import { ExternalLink } from './ExternalLink';
-import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
 
@@ -22,7 +21,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
           darkColor="rgba(255,255,255,0.05)"
           lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
+          <Text>{path}</Text>
         </View>
 
         <Text
