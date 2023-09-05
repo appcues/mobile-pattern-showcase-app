e59f68d851faac0d372662c0c10c50ed807bcf9f
Matt Hayashida <matt.hayashida@appcues.com>
2023-09-05 14:29:36 -0400

    🔧 Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: ['universe/native'],
+  rules: {
+    // sort members
+    'sort-imports': [
+      'error',
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+    // more intelligent sort of import statements
+    'import/order': ['error'],
+    'prettier/prettier': [
+      'error',
+      {
+        quoteProps: 'consistent',
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        useTabs: false,
+      },
+    ],
+  },
+};
