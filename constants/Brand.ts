aed8b74808f60ded0342d5a49a52c1ae4cf568b6
Matt Hayashida <matt.hayashida@appcues.com>
2023-09-05 14:28:21 -0400

    ✨ Add support for branded gradients

---
+++ constants/Brand.ts
@@ -0,0 +1,7 @@
+export const gradient = {
+  blurpleHaze: ['#5C5CFF', '#8960FF'],
+  moodLighting: ['#5C5CFF', '#8AD5FF'],
+  galaxy: ['#5C5CFF', '#FF92C6'],
+  tropico: ['#8960FF', '#FF5290'],
+  babyHaze: ['#DDDDFD', '#CFEEFF'],
+}
