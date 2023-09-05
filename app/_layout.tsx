1f6addf19ae4ef655317071fb35b2fb95544b6d9
Matt Hayashida <matt.hayashida@appcues.com>
2023-09-05 14:24:03 -0400

    ✨ Add Mulish font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,7 +20,22 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    'Mulish-Black': require('../assets/fonts/Mulish-Black.ttf'),
+    'Mulish-ExtraBold': require('../assets/fonts/Mulish-ExtraBold.ttf'),
+    'Mulish-Bold': require('../assets/fonts/Mulish-Bold.ttf'),
+    'Mulish-SemiBold': require('../assets/fonts/Mulish-SemiBold.ttf'),
+    'Mulish-Medium': require('../assets/fonts/Mulish-Medium.ttf'),
+    'Mulish-Regular': require('../assets/fonts/Mulish-Regular.ttf'),
+    'Mulish-Light': require('../assets/fonts/Mulish-Light.ttf'),
+    'Mulish-ExtraLight': require('../assets/fonts/Mulish-ExtraLight.ttf'),
+    'Mulish-BlackItalic': require('../assets/fonts/Mulish-BlackItalic.ttf'),
+    'Mulish-ExtraBoldItalic': require('../assets/fonts/Mulish-ExtraBoldItalic.ttf'),
+    'Mulish-BoldItalic': require('../assets/fonts/Mulish-BoldItalic.ttf'),
+    'Mulish-SemiBoldItalic': require('../assets/fonts/Mulish-SemiBoldItalic.ttf'),
+    'Mulish-MediumItalic': require('../assets/fonts/Mulish-MediumItalic.ttf'),
+    'Mulish-Italic': require('../assets/fonts/Mulish-Italic.ttf'),
+    'Mulish-LightItalic': require('../assets/fonts/Mulish-LightItalic.ttf'),
+    'Mulish-ExtraLightItalic': require('../assets/fonts/Mulish-ExtraLightItalic.ttf'),
     ...FontAwesome.font,
   });
 
