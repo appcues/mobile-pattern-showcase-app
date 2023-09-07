30bfa10455de22d0f404a58260512ef98836df28
Matt Hayashida <matt.hayashida@appcues.com>
2023-09-07 13:28:43 -0400

    💄 Ensure shadows render on Android

--- constants/Brand.ts
+++ constants/Brand.ts
@@ -20,23 +20,27 @@ export const shadow = {
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
+    elevation: 6,
   },
   elevation200: {
     shadowColor: color.neutral900,
     shadowOpacity: 0.16,
     shadowOffset: { width: 0, height: 4 },
     shadowRadius: 16,
+    elevation: 16,
   },
   elevation300: {
     shadowColor: color.neutral900,
     shadowOpacity: 0.2,
     shadowOffset: { width: 0, height: 12 },
     shadowRadius: 34,
+    elevation: 34,
   },
   elevation400: {
     shadowColor: color.neutral900,
     shadowOpacity: 0.35,
     shadowOffset: { width: 0, height: 12 },
     shadowRadius: 34,
+    elevation: 34,
   },
 };
