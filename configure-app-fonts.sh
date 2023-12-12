a5b55f0612a710e3113d6bf430826ef900f4729f
Matt Hayashida <matt.hayashida@appcues.com>
2023-12-12 13:36:55 -0500

    ✨ Set app display name to Appcues

--- configure-app-fonts.sh
+++ configure-app-fonts.sh
@@ -3,6 +3,11 @@
 # Assumes that source font files are located in ./assets/fonts
 
 if [[ "$EAS_BUILD_PLATFORM" == "android" ]]; then
+  # Note: the following 3 lines updating the app name is unrelated to font configuration and should not be copied.
+  echo "Update app name"
+  # on macos: -i ''
+  sed -i 's/Showcase/Appcues/' android/app/src/main/res/values/strings.xml
+
   echo "Copy fonts to resources"
   mkdir -p ./android/app/src/main/assets/fonts && cp ./assets/fonts/*.ttf "$_"
   ls -1 ./android/app/src/main/assets/fonts
